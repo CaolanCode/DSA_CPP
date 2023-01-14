3928f8052fd34a55e0d10502053b4f0d8bf38966
CaolanCode <cmacmahon3@gmail.com>
2023-01-14 12:34:24 +0000

    add functions for tasks

--- SpiralMatrix.cpp
+++ SpiralMatrix.cpp
@@ -7,11 +7,46 @@ using namespace std;
 
 void printVector(vector<int> &matrix, int count);
 vector<vector<int>> createMatrix();
+void SpiralMatrix(vector<vector<int>> matrix);
 
 int main()
 {
     vector<vector<int>> matrix = createMatrix();
+    SpiralMatrix(matrix);
     
+    return 0;
+}
+
+void printVector(vector<int> &matrix, int count)
+{
+    for(int i = 0; i < count; i++)
+    {
+        cout << matrix[i] << " ";
+    }
+    cout << endl;
+}
+
+vector<vector<int>> createMatrix()
+{
+    int rows, cols, index = 1;
+    
+    cout << "How many rows would you like in the vector? ";
+    cin >> rows;
+    cout << "How many columns would you like in the vector? ";
+    cin >> cols;
+    
+    vector<vector<int>> matrix(rows, vector<int>(cols));
+    
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < cols; j++) matrix[i][j] = index++;
+    }
+    
+    return matrix;
+}
+
+void SpiralMatrix(vector<vector<int>> matrix)
+{
     int startRow, endRow, startCol, endCol, count, m, n;
     startRow = startCol = count = 0;
     m = matrix.size();
@@ -65,38 +100,9 @@ int main()
             count++;
         }
         startCol++;
-        if(count == m*n) {
+        if(count == m * n) {
             printVector(newMatrix, count);
             break;
         }
     }
-    return 0;
-}
-
-void printVector(vector<int> &matrix, int count)
-{
-    for(int i = 0; i < count; i++)
-    {
-        cout << matrix[i] << " ";
-    }
-    cout << endl;
-}
-
-vector<vector<int>> createMatrix()
-{
-    int rows, cols, index = 1;
-    
-    cout << "How many rows would you like in the vector? ";
-    cin >> rows;
-    cout << "How many columns would you like in the vector? ";
-    cin >> cols;
-    
-    vector<vector<int>> matrix(rows, vector<int>(cols));
-    
-    for(int i = 0; i < rows; i++)
-    {
-        for(int j = 0; j < cols; j++) matrix[i][j] = index++;
-    }
-    
-    return matrix;
 }
