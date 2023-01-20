ca35b56b820de96fc9f690fddf5b305460e7b6a4
CaolanCode <cmacmahon3@gmail.com>
2023-01-20 06:49:37 +0000

    update repo

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -16,9 +16,52 @@ public:
     void append(int value);
     void prepend(int value);
     bool insert(int index, int value);
+    void printList();
+    void getHead() const;
+    void getTail() const;
+    void getLength() const;
 };
 
 LinkedList::LinkedList(int value)
 {
-    Node* newNode = new Node(value);
+    Node *newNode = new Node(value);
+    head = newNode;
+    tail = newNode;
+    length = 1;
+}
+
+void LinkedList::printList()
+{
+    Node *temp = head;
+    while(temp)
+    {
+        cout << temp->value << endl;
+        temp = temp->next;
+    }
+}
+
+void LinkedList::getHead() const
+{
+    cout << "Head: " << head->value << endl;
+}
+void LinkedList::getTail() const
+{
+    cout << "Tail: " << tail->value << endl;
+}
+void LinkedList::getLength() const
+{
+    cout << "Length: " << length << endl;
+}
+
+int main()
+{
+    LinkedList *myLinkedList = new LinkedList(4);
+    
+    myLinkedList->printList();
+    myLinkedList->getHead();
+    myLinkedList->getTail();
+    myLinkedList->getLength();
+    
+    
+    return 0;
 }
