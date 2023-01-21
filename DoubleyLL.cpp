c20df9049ac1f1c20806b8ac4a98b4a5ec4bc3a4
CaolanCode <cmacmahon3@gmail.com>
2023-01-21 12:13:28 +0000

    update doubly LL

--- DoubleyLL.cpp
+++ DoubleyLL.cpp
@@ -15,6 +15,10 @@ public:
     void append(int value);
     void deleteLast();
     void prepend(int value);
+    void deleteFirst();
+    Node *get(int index);
+    bool set(int index, int value);
+    bool insert(int index, int value);
     void printList();
     void getHead();
     void getTail();
@@ -65,6 +69,63 @@ void DoublyLinkedList::prepend(int value)
         head = newNode;
     }
     length++;
+}
+
+void DoublyLinkedList::deleteFirst()
+{
+    if(length == 0) return;
+    else if(length == 1) head = tail = nullptr;
+    else {
+        Node *temp = head;
+        head = head->next;
+        head->prev = nullptr;
+        delete temp;
+    }
+    length--;
+}
+
+Node* DoublyLinkedList::get(int index)
+{
+    if(index < length || index >= length) return nullptr;
+    Node *temp = head;
+    if(index < length/2) {
+        for(int i = 0; i < index; i++) temp = temp->next;
+    } else {
+        temp = tail;
+        for(int i = length-1; i > index; i--) temp = temp->prev;
+    }
+    return temp;
+}
+
+bool DoublyLinkedList::set(int index, int value)
+{
+    Node *temp = get(index);
+    if(temp) {
+        temp->value = value;
+        return true;
+    }
+    return false;
+}
+
+bool DoublyLinkedList::insert(int index, int value)
+{
+    if(index < 0 || index > length) return false;
+    else if(index == 0) {
+        prepend(value);
+        return true;
+    } else if(index == length - 1) {
+        append(value);
+        return true;
+    } else {
+        Node *newNode = new Node(value);
+        Node *before = get(index-1);
+        Node *after = before->next;
+        before->next = newNode;
+        after->prev = newNode;
+        newNode->prev = before;
+        newNode->next = after;
+    }
+    return true;
 }
 
 void DoublyLinkedList::printList()
