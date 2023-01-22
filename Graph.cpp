6823f3f39fb20bc31c3b810e212e416611cd8b43
CaolanCode <cmacmahon3@gmail.com>
2023-01-22 12:30:59 +0000

    commit graph

---
+++ Graph.cpp
@@ -0,0 +1,68 @@
+/*
+    Graph
+ */
+#include <iostream>
+#include <unordered_set>
+#include <unordered_map>
+using std::cout;
+using std::endl;
+using std::string;
+using std::unordered_set;
+using std::unordered_map;
+
+class Graph
+{
+    unordered_map<string, unordered_set<string> > adjList;
+public:
+    bool addVertex(string vertex);
+    bool addEdge(string vertex1, string vertex2);
+    bool removeEdge(string vertex1, string vertex2);
+    bool removeVertex(string vertex);
+};
+
+bool Graph::addVertex(string vertex)
+{
+    if(adjList.count(vertex) == 0) {
+        adjList[vertex];
+        return true;
+    }
+    return false;
+}
+
+bool Graph::addEdge(string vertex1, string vertex2)
+{
+    if(adjList.count(vertex1) != 0 && adjList.count(vertex2) != 0) {
+        adjList.at(vertex1).insert(vertex2);
+        adjList.at(vertex2).insert(vertex1);
+        return true;
+    }
+    return false;
+}
+
+bool Graph::removeEdge(string vertex1, string vertex2)
+{
+    if(adjList.count(vertex1) != 0 && adjList.count(vertex2) != 0) {
+        adjList.at(vertex1).erase(vertex2);
+        adjList.at(vertex2).erase(vertex1);
+        return true;
+    }
+    return false;
+}
+
+bool Graph::removeVertex(string vertex)
+{
+    if(adjList.count(vertex) == 0) return false;
+    for(auto otherVertex : adjList.at(vertex))
+    {
+        adjList.at(otherVertex).erase(vertex);
+    }
+    adjList.erase(vertex);
+    return true;
+}
+
+int main()
+{
+    Graph *myGraph = new Graph();
+    myGraph->addVertex("A");
+    return 0;
+}
