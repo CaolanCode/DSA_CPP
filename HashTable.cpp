b82a2b67cc924fea1dac1ced48b7a506e0552b80
CaolanCode <cmacmahon3@gmail.com>
2023-01-22 09:47:31 +0000

    commit hash table

--- HashTable.cpp
+++ HashTable.cpp
@@ -26,7 +26,46 @@ class HashTable
     Node *dataMap[SIZE];
 public:
     void printTable();
+    int hash(string key);
+    void set(string key, int value);
+    int get(string key);
 };
+
+int HashTable::hash(string key)
+{
+    int hash = 0;
+    for(int i = 0; i < key.length(); i++)
+    {
+        int ascciValue = int(key[i]);
+        hash = (hash + ascciValue * 23) % SIZE;
+    }
+    return hash;
+}
+
+void HashTable::set(string key, int value)
+{
+    int index = hash(key);
+    Node *newNode = new Node(key, value);
+    if(dataMap[index] == nullptr) dataMap[index] = newNode;
+    else {
+        Node *temp = dataMap[index];
+        while(temp->next)temp = temp->next;
+        temp->next = newNode;
+    }
+    
+}
+
+int HashTable::get(string key)
+{
+    int index = hash(key);
+    Node *temp = dataMap[index];
+    while(temp)
+    {
+        if(temp->key == key) return temp->value;
+        temp = temp->next;
+    }
+    return 0;
+}
 
 void HashTable::printTable()
 {
@@ -37,7 +76,7 @@ void HashTable::printTable()
             Node *temp = dataMap[i];
             while(temp)
             {
-                cout << " {" << temp->key << ", " << temp->value << "}";
+                cout << " {" << temp->key << ", " << temp->value << "}\n";
                 temp = temp->next;
             }
         }
@@ -47,6 +86,11 @@ void HashTable::printTable()
 int main()
 {
     HashTable *myHashTable = new HashTable();
+    myHashTable->set("Nails", 100);
+    myHashTable->set("Tile", 50);
+    myHashTable->set("Lumber", 80);
+    myHashTable->set("Bolts", 200);
+    myHashTable->set("Screws", 140);
     myHashTable->printTable();
     
     return 0;
