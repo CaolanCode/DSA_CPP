3bec80608f8fdbc23a65ca2431500037a2609b3d
CaolanCode <cmacmahon3@gmail.com>
2023-01-24 16:37:58 +0000

    commit contain duplicates

---
+++ ContainDuplicate.cpp
@@ -0,0 +1,41 @@
+/*
+    check if array contains duplicates
+ */
+#include <iostream>
+#include <unordered_set>
+
+using std::unordered_set;
+using std::cout;
+using std::endl;
+
+bool duplicates(int arr[], int arraySize);
+
+int main()
+{
+    int arr[] = {1,1,3,3,4,4,5,5};
+    int arraySize = sizeof(arr) / sizeof(arr[0]);
+    
+    if(duplicates(arr, arraySize)) {
+        cout << "Contains duplicates\n";
+    } else {
+        cout << "Doesn't contain duplicates\n";
+    }
+    
+    return 0;
+}
+
+bool duplicates(int arr[], int arraySize)
+{
+    unordered_set<int> st;
+    
+    for(int i = 0; i < arraySize; i++)
+    {
+        st.insert(arr[i]);
+    }
+    
+    int setSize = st.size();
+    
+    if(setSize < arraySize) return true;
+        
+    return false;
+}
